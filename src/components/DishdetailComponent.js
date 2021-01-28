2781990b2c18d9d2afc1643119553c078ae8766b
Emily Lu <emilylu123@gmail.com>
2021-01-28 17:06:46 +1030

    midify commets

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -13,34 +13,38 @@ import { Link } from "react-router-dom";
 function RenderDish({ dish }) {
   if (dish != null)
     return (
-      <Card>
-        <CardImg top src={dish.image} alt={dish.name} />
-        <CardBody>
-          <CardTitle>{dish.name}</CardTitle>
-          <CardText>{dish.description}</CardText>
-        </CardBody>
-      </Card>
+      <div className="col-12 col-md-5 m-1">
+        <Card>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      </div>
     );
   else return <div />;
 }
 
-function RenderComments({ dish }) {
-  if (dish != null) {
+function RenderComments({ comments }) {
+  if (comments != null) {
     return (
-      <div>
+      <div className="col-12 col-md-5 m-1">
         <h4>Comments</h4>
-        {dish.comments.map((comment, i) => {
-          return (
-            <div key={comment.id}>
-              <p>{comment.comment}</p>
-              <p>{`-- ${comment.author} , ${new Intl.DateTimeFormat("en-US", {
-                year: "numeric",
-                month: "short",
-                day: "2-digit",
-              }).format(new Date(Date.parse(comment.date)))}`}</p>
-            </div>
-          );
-        })}
+        <ul className="list-unstyled">
+          {comments.map((comment) => {
+            return (
+              <li key={comment.id}>
+                <p>{comment.comment}</p>
+                <p>{`-- ${comment.author} , ${new Intl.DateTimeFormat("en-US", {
+                  year: "numeric",
+                  month: "short",
+                  day: "2-digit",
+                }).format(new Date(Date.parse(comment.date)))}`}</p>
+              </li>
+            );
+          })}
+        </ul>
         ;
       </div>
     );
@@ -66,12 +70,8 @@ const DishDetail = (props) => {
           </div>
         </div>
         <div className="row">
-          <div className="col-12 col-md-5 m-1">
-            <RenderDish dish={props.dish} />
-          </div>
-          <div className="col-12 col-md-5 m-1">
-            <RenderComments comments={props.comments} />
-          </div>
+          <RenderDish dish={props.dish} />
+          <RenderComments comments={props.comments} />
         </div>
       </div>
     );
