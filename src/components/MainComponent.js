1840a2e33e96099f0c9c5d033a9efb2f8a1c87d0
Emily Lu <emilylu123@gmail.com>
2021-01-28 16:55:46 +1030

    Single Page Applications Part 2  - parameter with router

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -22,6 +22,7 @@ class Main extends Component {
       leaders: LEADERS,
     };
   }
+
   //   onDishSelect(dishId) {
   //     this.setState({ selectedDish: dishId });
   //   }
@@ -36,6 +37,23 @@ class Main extends Component {
         />
       );
     };
+
+    //   Use Parameters on Routes
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail
+          dish={
+            this.state.dishes.filter(
+              (dish) => dish.id === parseInt(match.params.dishId, 10)
+            )[0]
+          }
+          comments={this.state.comments.filter(
+            (comment) => comment.dishId === parseInt(match.params.dishId, 10)
+          )}
+        />
+      );
+    };
+
     return (
       <div>
         <Header />
@@ -47,6 +65,7 @@ class Main extends Component {
             path="/menu"
             component={() => <Menu dishes={this.state.dishes} />}
           />
+          <Route path="/menu/:dishId" component={DishWithId} />
           <Route exact path="/contactus" component={Contact} />
           <Redirect to="/home" />
         </Switch>
--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,16 +1,24 @@
 import React from "react";
-import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardTitle,
+  Breadcrumb,
+  BreadcrumbItem,
+} from "reactstrap";
+import { Link } from "react-router-dom";
 
-// Turn into a functional component
 function RenderMenuItem({ dish, onClick }) {
   return (
-    // delete onclick
-    // <Card key={dish.id} onClick={() => onClick(dish.id)}>
-    <Card key={dish.id}>
-      <CardImg width="100%" src={dish.image} alt={dish.name} />
-      <CardImgOverlay>
-        <CardTitle>{dish.name}</CardTitle>
-      </CardImgOverlay>
+    <Card>
+      {/* Use Parameters on Routes */}
+      <Link to={`/menu/${dish.id}`}>
+        <CardImg width="100%" src={dish.image} alt={dish.name} />
+        <CardImgOverlay>
+          <CardTitle>{dish.name}</CardTitle>
+        </CardImgOverlay>
+      </Link>
     </Card>
   );
 }
@@ -26,6 +34,18 @@ const Menu = (props) => {
 
   return (
     <div className="container">
+      <div className="row">
+        <Breadcrumb>
+          <BreadcrumbItem>
+            <Link to="/home">Home</Link>
+          </BreadcrumbItem>
+          <BreadcrumbItem active>Menu</BreadcrumbItem>
+        </Breadcrumb>
+        <div className="col-12">
+          <h3>Menu</h3>
+          <hr />
+        </div>
+      </div>
       <div className="row">{menu}</div>
     </div>
   );
